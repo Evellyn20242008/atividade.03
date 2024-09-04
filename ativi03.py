d9e0b3c0d1e2f192d188e020340f402d6f3b6e24
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-04 15:52:28 -0300

    area

---
+++ ativi03.py
@@ -0,0 +1,7 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+l=int(input("digite a largura"))
+at=int(input("digite a altura"))
+
+soma= l*at
+
+print=(f"a somada da area é = {soma}")
